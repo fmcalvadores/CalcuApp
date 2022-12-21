6ff4f0fb511df5b00bed0c96f6b96ba09d3e3a8a
Francis Calvadores <fmcalvadores.beehive@gmail.com>
2022-12-21 08:48:31 +0800

    added sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,19 +11,30 @@ if (wwOpenInstalled || parent.wwOpenInstalled) {
 function loadTermsInYear()
 {
 	var terms = document.getElementById("terms");
-	var maxYear = 30;
+	var maxYear = 32;
 	for(var i=0; i <= maxYear; i++)
 	{
 		var option = document.createElement('option');
+
 		if(i == 0)
 		{
-			option.value = "";
+			option.value = i;
 			option.innerHTML = "-- Please select a term --";
+		}
+		else if(i == 31)
+		{
+			option.value = 3;
+			option.innerHTML = 3 + " months";
+		}
+		else if(i == 32)
+		{
+			option.value = 6;
+			option.innerHTML = 6 + " months";
 		}
 		else 
 		{
 			option.value = i * 12;
-			option.innerHTML = i;
+			option.innerHTML = i + (i == 1 ? " year" : " years");
 		}
 		
 		terms.appendChild(option);
@@ -39,19 +50,17 @@ function getValues()
 	
 	var div = document.getElementById("Result");
 	div.innerHTML = "";
-	var balVal = validateInputs(balance);
-	var intrVal = validateInputs(interestRate);
-    var trmVal = validateInputs(terms);
+	var balVal = validateInputs(document.getElementById("principal").name,balance);
+	var intrVal = validateInputs(document.getElementById("interest").name,interestRate);
+    var trmVal = validateInputs(document.getElementById("terms").name,terms);
 
 	if (balVal && intrVal)
 	{
 		div.innerHTML += calculateAmortization(balance, interestRate, terms);
-		document.getElementById("loanBreakdown").style.display = 'block';
 	}
 	else
 	{
 		div.innerHTML += "Please Check your inputs and retry - invalid values.";
-		document.getElementById("loanBreakdown").style.display = 'none';
 	}
 }
 
@@ -71,11 +80,11 @@ function calculateAmortization(balance, interestRate, terms)
 	var monthlyRate = interestRate / 12;
     var payment = balance * (monthlyRate/(1 - Math.pow(1 + monthlyRate, -terms)));
 	    
-    var result = "Loan amount: $" + balance.toFixed(2) +  "<br />" + 
-        "Interest rate: " + (interestRate*100).toFixed(2) +  "%<br />" +
-        "Number of months: " + terms + "<br />" +
-        "Monthly payment: $" + payment.toFixed(2) + "<br />" +
-        "Total paid: $" + (payment * terms).toFixed(2) + "<br /><br />";
+    var result = "Loan Amount: "  + balance.toFixed(2) +  "<br />" + 
+        "Interest Rate: " + (interestRate*100).toFixed(2) +  "%<br />" +
+        "Number of Months: " + terms + "<br />" +
+        "Monthly Payment: " + payment.toFixed(2) + "<br />" +
+        "Total Paid: " + (payment * terms).toFixed(2) + "<br /><br />";
         
 	result += "<table class='table'>" +
 				 "<thead>"+
@@ -91,11 +100,11 @@ function calculateAmortization(balance, interestRate, terms)
 		var monthlyPrincipal = 0;
 		result += "<tr>";
 		result += "<td>" + (count + 1) + "</td>";
-		result += "<td> $" + balance.toFixed(2) + "</td>";
+		result += "<td>" + balance.toFixed(2) + "</td>";
 		interest = balance * monthlyRate;
-		result += "<td> $" + interest.toFixed(2) + "</td>";
+		result += "<td>" + interest.toFixed(2) + "</td>";
 		monthlyPrincipal = payment - interest;
-		result += "<td> $" + monthlyPrincipal.toFixed(2) + "</td>";
+		result += "<td>" + monthlyPrincipal.toFixed(2) + "</td>";
 		result += "</tr>";
 		balance = balance - monthlyPrincipal;		
 	}
@@ -103,10 +112,11 @@ function calculateAmortization(balance, interestRate, terms)
     return result;
 }
 
-function validateInputs(value)
+function validateInputs(field, value)
 {
 	if ((value == null) || (value == ""))
 	{
+
 		return false;
 	}
 	return true;
